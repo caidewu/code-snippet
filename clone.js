c26c763cc2f2f9f294fde761c0e736d7eab80a6f
caidewu <caidewu555@gmail.com>
2017-03-10 22:38:55 +0800

    继承方法中 考虑ES5的属性描述

--- clone.js
+++ clone.js
@@ -18,6 +18,43 @@ function deepClone(arr) {
     return JSON.parse(JSON.stringify(arr));
 }
 
+
+function extend(deep, target, source) {
+    for (var i in source) {
+        if (deep) {
+
+        } else {
+            if (source.hasOwnProperty(i)) { // 不要把原型上的属性拷贝过来
+                target[i] = source[i];
+            }
+        }
+    }
+}
+
+// 上面的方法并没有考虑到ES5属性描述的情况，也就是说如果有getter setter或对象被freeze了，这些特性是拷贝不过来的
+function extend1(deep, target, source) {
+    function isDescUse (obj) {
+        return !obj.writable || !obj.enumerable || !obj.configurable || obj.get || obj.set;
+    }
+    for (var i in source) {
+        if (deep) {
+            // 这里就是递归了
+            // 要注意先判断Array，然后Object，基本类型和function直接拷贝
+        } else {
+            if (source.hasOwnProperty(i)) { // 不要把原型上的属性拷贝过来
+                var description = Object.getOwnPropertyDescriptor(source, i);
+
+                if (description && isDescUse()) {
+                    Object.defineProperty(target, i, description);
+                } else {
+                    target[i] = source[i];
+                }
+            }
+        }
+    }
+}
+
+
 var arr = arrayGenerator(10000);
 
 
